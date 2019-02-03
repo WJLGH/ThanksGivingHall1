e20faed35fbb93133fb9d976fd15fe22c3f2d7d9
wjl <1179315470@qq.com>
2019-02-03 12:41:34 +0800

    2019/02/03 修复默认IP地址和端口为空的BUG

--- app/src/main/java/com/example/sxd/thanksgivinghall/FinanceStartActivity.java
+++ app/src/main/java/com/example/sxd/thanksgivinghall/FinanceStartActivity.java
@@ -1,6 +1,7 @@
 
 package com.example.sxd.thanksgivinghall;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -11,11 +12,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.sxd.thanksgivinghall.bean.Constants;
+import com.example.sxd.thanksgivinghall.finance.FinRecordAddActivity;
+import com.example.sxd.thanksgivinghall.finance.FinRecordDetailActivity;
+import com.example.sxd.thanksgivinghall.login.SettingActivity;
+import com.example.sxd.thanksgivinghall.utils.SharedPreUtils;
+
 import java.util.LinkedList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FinanceStartActivity extends AppCompatActivity {
 
@@ -26,6 +34,8 @@ public class FinanceStartActivity extends AppCompatActivity {
     Button btnDetail;
     @BindView(R.id.bt_fin_start_list)
     Button btnList;
+    @BindView(R.id.bt_fin_start_set)
+    Button btnSet;
     @BindView(R.id.vp_fin_start)
     ViewPager viewPager ;
     @Override
@@ -33,7 +43,35 @@ public class FinanceStartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance_start);
         ButterKnife.bind(this);
-        initViewPager();
+        initIPAndPort();
+//        initViewPager();
+    }
+    public void initIPAndPort() {
+        SharedPreUtils.putString(this,Constants.SP_IP,"10.0.2.2");
+        SharedPreUtils.putString(this,Constants.SP_PORT,"8080");
+    }
+
+    @OnClick({R.id.bt_fin_start_add,R.id.bt_fin_start_detail,R.id.bt_fin_start_list,R.id.bt_fin_start_set})
+    public void intentAct(View v) {
+        switch (v.getId()) {
+            case R.id.bt_fin_start_add:
+                Intent intent = new Intent(FinanceStartActivity.this,FinRecordAddActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.bt_fin_start_detail:
+                Intent intent1 = new Intent(FinanceStartActivity.this,FinRecordDetailActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.bt_fin_start_set:
+                Intent intent3 = new Intent(FinanceStartActivity.this,SettingActivity.class);
+                startActivity(intent3);
+                break;
+//            case R.id.bt_fin_start_list:
+//                Intent intent2 = new Intent(FinanceStartActivity.this,FinRecordListActivity.class);
+//                startActivity(intent2);
+//                break;
+        }
+
     }
     public void initViewPager() {
         final List<View> list = new LinkedList<>();
